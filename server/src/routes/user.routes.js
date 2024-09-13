2ba086be606ec0c2f5b078bca0bea752aadf973c
Zeeshan <zeeshanalianjam49@gmail.com>
2024-09-13 11:03:47 +0500

    add more api's in userControllers

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import { addBook, getUserInforamtion, signInUser, signUpUser, updateAddress, updateBook } from '../controllers/user.controllers.js'
+import { addBook, deleteBook, getAllBooks, getBookById, getRecentlyAddedBooks, getUserInforamtion, signInUser, signUpUser, updateAddress, updateBook } from '../controllers/user.controllers.js'
 import { authenticateToken } from '../middlewears/userAuth.middlewears.js'
 
 export const router = Router()
@@ -10,3 +10,7 @@ router.route("/get-user-information").get(authenticateToken, getUserInforamtion)
 router.route("/update-address").put(authenticateToken, updateAddress)
 router.route("/add-book").post(authenticateToken, addBook)
 router.route("/update-book").put(authenticateToken, updateBook)  
+router.route("/delete-book").delete(authenticateToken, deleteBook)
+router.route("/get-all-books").get(getAllBooks)
+router.route("/get-recent-books").get(getRecentlyAddedBooks)
+router.route("/get-book-by-id/:id").get(getBookById)
